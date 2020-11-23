cd1de2cfdd425ce32c8aa538672738a961e070e1
Morgan-dev311 <morganevansdemongirl@gmail.com>
2020-11-23 20:02:15 +0400

    Add autodispose

--- app/src/main/java/ru/commandos/diner/delivery/controller/OrdersController.java
+++ app/src/main/java/ru/commandos/diner/delivery/controller/OrdersController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import autodispose2.AutoDispose;
+import io.reactivex.rxjava3.core.CompletableObserver;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import retrofit2.Response;
@@ -61,7 +63,9 @@ public class OrdersController {
 
     public void acceptOrder() {
         Optional.ofNullable(incomingOrder).ifPresent(order -> {
-                    compositeDisposable.add(serverApi.acceptOrder(courierUuid, order.getUuid()).doOnComplete(compositeDisposable::dispose).subscribe(this::updateOrderList));
+                    serverApi.acceptOrder(courierUuid, order.getUuid())
+                            .to(AutoDispose.autoDisposable(CompletableObserver::onComplete))
+                            .subscribe(this::updateOrderList);
                     acceptedOrders.add(order);
                 }
         );
@@ -70,7 +74,10 @@ public class OrdersController {
 
     public void denyOrder() {
         Optional.ofNullable(incomingOrder).ifPresent(order -> {
-                    compositeDisposable.add(serverApi.denyOrder(courierUuid, order.getUuid()).doOnComplete(compositeDisposable::dispose).subscribe(this::updateOrderList));
+                    serverApi.denyOrder(courierUuid, order.getUuid())
+                            .to(AutoDispose.autoDisposable(CompletableObserver::onComplete))
+                            .subscribe(this::updateOrderList);
+                    acceptedOrders.add(order);
                 }
         );
         incomingOrder = null;
